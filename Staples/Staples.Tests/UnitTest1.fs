651b3c9b63001f5f7c9c429f3236302d9d5e9af6
Azernem <Azernem@gmail.com>
2025-03-12 23:16:42 +0300

    added program

---
+++ Staples/Staples.Tests/UnitTest1.fs
@@ -0,0 +1,11 @@
+module Staples.Tests
+
+open NUnit.Framework
+
+[<SetUp>]
+let Setup () =
+    ()
+
+[<Test>]
+let Test1 () =
+    Assert.Pass()
---
+++ Staples/Staples/Program.fs
@@ -0,0 +1,21 @@
+﻿namespace Staples
+module Staples = 
+    let openingStaples = ['('; '{'; '[']
+    let closingStaples = [')'; '}'; ']']
+
+    let rec areCorrectStaples expression checkList =  
+        match expression with 
+        | [] -> true
+        | x :: tail -> 
+            if List.contains x openingStaples then areCorrectStaples tail (x :: checkList)
+            elif List.contains x closingStaples then 
+                match checkList with 
+                | [] -> false
+                | char :: ls -> 
+                    if (List.tryFindIndex (fun i -> i = x) closingStaples) = (List.tryFindIndex (fun i -> i = char) openingStaples) then areCorrectStaples tail ls
+                    else false
+            else areCorrectStaples tail checkList
+
+        
+
+         
