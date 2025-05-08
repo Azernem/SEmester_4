7f0a05350dd8c9a26be055f71de8677cdf1a2681
Azernem <Azernem@gmail.com>
2025-05-08 19:58:53 +0300

    fix error and the more tests

--- Phone_Directory/Phone.Tests/UnitTest1.fs
+++ Phone_Directory/Phone.Tests/UnitTest1.fs
@@ -22,8 +22,11 @@ type UnitTests () =
     member this.``Tests write and print contact`` () = 
         let book = []
         let newBook = addContact "Jone" "566577" book
-        newBook[0] |> should equal ("Jone", "566577")
-        newBook |> should haveLength 1
+        let path = "test_contacts.txt"
+        writeContactsToFile newBook path
+        let loaded = printContacts path
+        loaded |> should contain ("Jone", "566577")
+        if System.IO.File.Exists(path) then System.IO.File.Delete(path)
 
     [<Test>]
     member this.``Tests find number`` () = 
@@ -32,3 +35,9 @@ type UnitTests () =
         let newBook = addContact "Jack" "775665" newBook
         findNumber "Jack" newBook |> should equal (Some "775665")
         newBook |> should haveLength 2
+
+    [<Test>]
+    member this.``Tests print contacts from nonexistent file`` () =
+        let path = "nonexistent_file.txt"
+        let loaded = printContacts path
+        loaded |> should equal []
--- Phone_Directory/Phone/Program.fs
+++ Phone_Directory/Phone/Program.fs
@@ -1,23 +1,28 @@
 namespace Phone
 open System.IO
+
+/// The phone directory
 module Directory = 
+    /// add contacts
     let addContact name number book = 
         (name, number) :: book
     
+    /// find number about name
     let findNumber name book = List.tryFind (fun i -> i |> fst = name) book |> function
         | Some a -> Some (snd a)
         | None -> None
     
+    /// find name about number
     let findName number book = List.tryFind (fun i -> i |> snd = number) book |> function
-        | Some a -> Some (snd a)
+        | Some a -> Some (fst a)
         | None -> None
     
+    ///write contacts to file
     let writeContactsToFile book path =
-        if File.Exists(path) then
-            book |> List.map (fun i -> (i |> fst) + "-" + (i |> snd)) |> Array.ofList 
-            |> fun list -> File.WriteAllLines(path, list)
-        else failwithf "doesnt exists file"
-    
+        book |> List.map (fun i -> (i |> fst) + "-" + (i |> snd)) |> Array.ofList 
+        |> fun list -> File.WriteAllLines(path, list)
+
+    /// print contacts
     let printContacts path = 
         if File.Exists(path) then
             File.ReadAllLines(path) |> Array.toList |> List.map (fun line -> (line.Split('-')[0], line.Split('-')[1]))
