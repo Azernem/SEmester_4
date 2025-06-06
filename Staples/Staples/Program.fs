f7699d0108f8a24ef6ef4873210a7e6806e23239
Azernem <Azernem@gmail.com>
2025-06-06 15:03:56 +0300

    last

--- Staples/Staples/Program.fs
+++ Staples/Staples/Program.fs
@@ -3,18 +3,22 @@ module Staples =
     let openingStaples = ['('; '{'; '[']
     let closingStaples = [')'; '}'; ']']
 
-    let rec areCorrectStaples expression checkList =  
-        match expression with 
-        | [] -> true
-        | x :: tail -> 
-            if List.contains x openingStaples then areCorrectStaples tail (x :: checkList)
-            elif List.contains x closingStaples then 
-                match checkList with 
-                | [] -> false
-                | char :: ls -> 
-                    if (List.tryFindIndex (fun i -> i = x) closingStaples) = (List.tryFindIndex (fun i -> i = char) openingStaples) then areCorrectStaples tail ls
-                    else false
-            else areCorrectStaples tail checkList
+    let areCorrectStaples expression =
+        let rec recAreCorrectStaples expression checkList =  
+            match expression with 
+            | [] -> List.isEmpty checkList
+            | x :: tail -> 
+                if List.contains x openingStaples then 
+                    recAreCorrectStaples tail (x :: checkList)
+                elif List.contains x closingStaples then 
+                    match checkList with 
+                    | [] -> false
+                    | char :: ls -> 
+                        if (List.tryFindIndex (fun i -> i = x) closingStaples) = (List.tryFindIndex (fun i -> i = char) openingStaples) then recAreCorrectStaples tail ls
+                        else false
+                else 
+                    recAreCorrectStaples tail checkList
+        recAreCorrectStaples expression []
 
         
 
