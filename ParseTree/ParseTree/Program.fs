f2081695ac459901ec0f826f800b82614e11c543
Azernem <nemat.musaev5@mail.ru>
2025-09-13 22:09:59 +0400

    Complexity has reduced according linearization with tail recursion, added license

--- ParseTree/ParseTree/Program.fs
+++ ParseTree/ParseTree/Program.fs
@@ -1,16 +1,24 @@
-﻿namespace ParseTree
+﻿// <copyright file="Program.cs" company="NematMusaev">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace ParseTree
 
 module ParseTree =
+
+// class of sign, operations in tree calculating
     type Sign =
         | Plus
         | Minus
         | Multiply
         | Divide
 
+// class of tree
     type Tree =
         | Operand of int
         | Node of Sign * Tree * Tree
 
+// Parse tree, calculates and fetches result for root
     let rec parse tree =
         match tree with
         | Operand(n) -> n
@@ -20,3 +28,23 @@ module ParseTree =
             | Minus -> parse left - parse right
             | Multiply -> parse left * parse right
             | Divide -> if parse right <> 0 then parse left / parse right else failwith "Division by zero"
+
+//Parse tree, calculates and fetches result for root in CPS style
+    let parseCPS tree =
+        let rec parseCPSin tree cont =
+            match tree with
+            | Operand(n) -> cont n
+            | Node(sign, left, right) ->
+                parseCPSin left (fun leftResult ->
+                    parseCPSin right (fun rightResult ->
+                        match sign with
+                        | Plus -> cont (leftResult + rightResult)
+                        | Minus -> cont (leftResult - rightResult)
+                        | Multiply -> cont (leftResult * rightResult)
+                        | Divide -> 
+                            if rightResult <> 0 then cont (leftResult / rightResult)
+                            else failwith "Division by zero"
+                    )
+                )
+    
+        parseCPSin tree id
