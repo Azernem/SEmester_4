7b06d8d5d6f4357d11dbb05ab4351500605f62a3
Azernem <nemat.musaev5@mail.ru>
2025-09-13 21:26:22 +0400

    complexity reduced according linealization, added tail recursion

--- MapForTree/MapForTree.Tests/MapForTree.Tests.fs
+++ MapForTree/MapForTree.Tests/MapForTree.Tests.fs
@@ -5,10 +5,6 @@ open FsUnit
 open FsCheck
 open MapTree
 
-[<SetUp>]
-let Setup () =
-    ()
-
 [<TestFixture>]
 type TreeTests () =
     [<Test>]
@@ -30,11 +26,29 @@ type TreeTests () =
         mapTree (fun x -> x * 2) tree |> should equal checkTree
 
     [<Test>]
+    member this.``Test mapTreeCPS`` () =
+        let tree: Tree<int> = 
+            Node (3,
+                Node (9,
+                        Node (1, Empty, Empty),
+                        Node (2, Empty, Empty)),
+                Node (11, Empty, Empty))
+
+        let checkTree: Tree<int> =
+            Node (6,
+                Node (18,
+                        Node (2, Empty, Empty),
+                        Node (4, Empty, Empty)),
+                Node (22, Empty, Empty))
+
+        mapTreeCPS (fun x -> x * 2) tree |> should equal checkTree
+
+    [<Test>]
     member this.TestRightIntoHerSelf () =
         let GoIntoHerSelf tree =
-            mapTree (fun x -> x) tree = tree
+            mapTree id tree = tree
             
-        Check.Quick (fun (tree: Tree<string>) -> GoIntoHerSelf tree)
-        Check.Quick (fun (tree: Tree<int>) -> GoIntoHerSelf tree)
+        Check.QuickThrowOnFailure (fun (tree: Tree<string>) -> GoIntoHerSelf tree)
+        Check.QuickThrowOnFailure (fun (tree: Tree<int>) -> GoIntoHerSelf tree)
 
 
