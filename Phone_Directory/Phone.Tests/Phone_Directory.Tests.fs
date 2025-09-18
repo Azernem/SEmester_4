5661f339ced8e59742b10bbf71708aa1999989cc
Azernem <nemat.musaev5@mail.ru>
2025-09-19 01:03:30 +0400

    interactive mode for phone using (load, find, save...)

---
+++ Phone_Directory/Phone.Tests/Phone_Directory.Tests.fs
@@ -0,0 +1,31 @@
+module Phone.Tests
+
+open NUnit.Framework
+open FsUnit
+open Phone.Directory
+
+[<Test>]
+let ``Tests Add Contact`` () = 
+    let book = []
+    let newBook = addContact "Jone" "566577" book
+    newBook[0] |> should equal ("Jone", "566577")
+    newBook |> should haveLength 1
+
+[<Test>]
+let ``Tests write and print contact`` () = 
+    let book = []
+    let newBook = addContact "Jone" "566577" book
+    let path = "test_contacts.txt"
+    writeContactsToFile newBook path
+    let loaded = printContacts path
+    loaded |> should contain ("Jone", "566577")
+    if System.IO.File.Exists(path) then System.IO.File.Delete(path)
+
+[<Test>]
+let ``Tests find number`` () = 
+    let book = []
+    let newBook = addContact "Jone" "566577" book
+    let newBook = addContact "Jack" "775665" newBook
+    findNumber "Jack" newBook |> should equal (Some "775665")
+    newBook |> should haveLength 2
+
--- Phone_Directory/Phone/Program.fs
+++ Phone_Directory/Phone/Program.fs
@@ -1,29 +1,92 @@
 namespace Phone
 open System.IO
+open System
 
 /// The phone directory
 module Directory = 
-    /// add contacts
+    /// Add contacts
     let addContact name number book = 
         (name, number) :: book
     
-    /// find number about name
+    /// find number by name
     let findNumber name book = List.tryFind (fun i -> i |> fst = name) book |> function
         | Some a -> Some (snd a)
         | None -> None
     
-    /// find name about number
+    /// find name by number
     let findName number book = List.tryFind (fun i -> i |> snd = number) book |> function
         | Some a -> Some (fst a)
         | None -> None
     
-    ///write contacts to file
+    ///Write contacts to file
     let writeContactsToFile book path =
-        book |> List.map (fun i -> (i |> fst) + "-" + (i |> snd)) |> Array.ofList 
+        book |> List.map (fun (name, number) -> name + "-" + number) |> Array.ofList 
         |> fun list -> File.WriteAllLines(path, list)
 
-    /// print contacts
+    /// Print contacts
     let printContacts path = 
         if File.Exists(path) then
-            File.ReadAllLines(path) |> Array.toList |> List.map (fun line -> (line.Split('-')[0], line.Split('-')[1]))
+            File.ReadAllLines(path) |> Array.toList |> List.map (fun line ->  (line.Split('-')[0], line.Split('-')[1]))
         else []
+
+/// Interactive process of phone using
+module Interactive = 
+    let run() = 
+        let path = "../Details/phone.txt"
+        printfn "Phone Directory with Interactive Mode"
+        printfn "Commands:"
+        printfn "  add <name> <number> - Add contact"
+        printfn "  findname <number>   - Find name by number"
+        printfn "  findnumber <name>   - Find number by name"
+        printfn "  list                - Show contacts"
+        printfn "  save                - Save contacts to file"
+        printfn "  load                - Load contacts from file" 
+        printfn "  exit                - Exit program"
+      
+        let rec processRequest book = 
+            printf "> "
+            let input = Console.ReadLine().Trim()
+            match input.Split(" ") |> Array.filter(fun el -> el <> "") with
+                | [|"exit"|] -> 
+                    printfn "Working with phone is completed"  
+                    book
+
+                | [|"add"; name; number|] ->
+                    let newBook = Directory.addContact name number book 
+                    printfn "Contact added: %s - %s" name number
+                    processRequest newBook 
+                    
+                | [|"findname"; number|] ->
+                    match Directory.findName number book with
+                    | Some name -> printfn "Name: %s" name
+                    | None -> printfn "Number not found"
+                    processRequest book
+                
+                | [|"findnumber"; name|] ->
+                    match Directory.findNumber name book with
+                    | Some number -> printfn "Number: %s" number
+                    | None -> printfn "Not found number for this person" 
+                    processRequest book
+                
+                | [|"load"|] -> 
+                    let loadedBook = Directory.printContacts path
+                    printfn "Contacts loaded from file"
+                    processRequest loadedBook
+
+                | [|"list"|] ->
+                    if List.isEmpty book then 
+                        printfn "Database is empty"
+                    else
+                        book |> List.iter (fun (name, number) -> printfn "%s - %s" name number)  // Добавил n
+                    processRequest book
+                
+                | [|"save"|] ->
+                    Directory.writeContactsToFile book path
+                    printfn "Contacts saved to %s" path
+                    processRequest book
+
+                | _ ->  
+                    printfn "Unknown command. Type 'exit' to quit."
+                    processRequest book
+
+        processRequest []  
